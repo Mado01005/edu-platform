dfdf1b6f683807a8ac5826f44a9aa687ee95b224
abdallah saad <abdallahsaad@abdallahs-MacBook-Air.local>
2026-03-26 18:37:02 +0200

    fix: resolve production configuration error by adding trustHost

--- src/auth.ts
+++ src/auth.ts
@@ -8,6 +8,7 @@ import { ADMIN_EMAILS } from '@/lib/constants';
 export const { handlers, auth, signIn, signOut } = NextAuth({
   debug: true,
   secret: process.env.AUTH_SECRET,
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
